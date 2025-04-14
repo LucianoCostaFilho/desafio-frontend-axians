4d5ab6c68917c5ea354e54c4cbe1837732e7e20a
lucianoCostaFilho <luciano.costa.filho92@gmail.com>
2025-04-14 20:42:03 -0300

    feature ajustando taxTable

--- Models/Taxa.cs
+++ Models/Taxa.cs
@@ -6,6 +6,10 @@ namespace DesafioAxians.Models
         public decimal Valor { get; set; }
         public bool Selecionada { get; set; }
 
+        public string Nuts2 { get; set; } = string.Empty;
+        public string Nuts3 { get; set; } = string.Empty;
+        public string Concelho { get; set; } = string.Empty;
+
         public Taxa(string tipo, decimal valor)
         {
             Tipo = tipo;
--- obj/Debug/net9.0/DesafioAxians.AssemblyInfo.cs
+++ obj/Debug/net9.0/DesafioAxians.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioAxians")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5956fb1e87589c065a176dfc9f243bb244ff1d4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+640be27aa27ea4cc9f1ea5476bdf2cf1641b229f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioAxians")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioAxians")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
